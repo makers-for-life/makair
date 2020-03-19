afc4cec57508d101486c73a0b90c18aa63ba140c
David Sferruzza <david.sferruzza@gmail.com>
2020-03-19 23:51:40 +0100

    Fix coding style & add inline documentation

--- src/respi_v0/respi_v0.ino
+++ src/respi_v0/respi_v0.ino
@@ -4,64 +4,64 @@ Servo patient;
 Servo blower;
 
 const int PIN_CAPTEUR_PRESSION = 4; // A4
-const int  PIN_SERVO_BLOWER = 4; // D4
-const int  PIN_SERVO_PATIENT = 2; // D2
+const int PIN_SERVO_BLOWER = 4; // D4
+const int PIN_SERVO_PATIENT = 2; // D2
 
-const int  PHASE_PUSH_INSPI = 1; // première phase où on envoie l'air jusqu'à pression crete param : valve blower ouverte à consigne, valve patient fermée (pas d'expi) 
-const int  PHASE_HOLD_INSPI = 2; // plateau : on a depassé la pression crete, la pression descend depuis plus d'un 1/10sec (arbitraire EFL) : 2 valve fermée
-const int  PHASE_EXPIRATION = 3; // Expiration : valve blower 
+const int PHASE_PUSH_INSPI = 1; // inspiration : on envoie l'air jusqu'à la pression crête paramétrée : valve blower ouverte à consigne, valve patient fermée (pas d'expiration)
+const int PHASE_HOLD_INSPI = 2; // plateau : on a depassé la pression crête, la pression descend depuis plus d'un 1/10sec (arbitraire EFL) : 2 valves fermées
+const int PHASE_EXPIRATION = 3; // expiration : valve blower fermée, valve patient ouverte
 
-const int  ANGLE_OUVERTURE_MINI = 8;
-const int  ANGLE_OUVERTURE_MAXI = 45;
+const int ANGLE_OUVERTURE_MINI = 8;
+const int ANGLE_OUVERTURE_MAXI = 45;
 
-int secu_coupureBrower = 45;
+int secu_coupureBlower = 45;
 int secu_ouvertureExpi = 45;
 
 void setup() {
-//  Serial.begin(115200); 
-//  Serial.println("demarrage");
-patient.attach(PIN_SERVO_PATIENT);
-blower.attach(PIN_SERVO_BLOWER);
+  //Serial.begin(115200);
+  //Serial.println("demarrage");
+  patient.attach(PIN_SERVO_PATIENT);
+  blower.attach(PIN_SERVO_BLOWER);
 
-//Serial.print("mise en secu initiale");
-blower.write(secu_coupureBrower);
-patient.write(secu_ouvertureExpi);
-
+  //Serial.print("mise en secu initiale");
+  blower.write(secu_coupureBlower);
+  patient.write(secu_ouvertureExpi);
 }
-// nombre de cycle par minute (cycle = inspi + expi)
+
+// nombre de cycles par minute (cycle = inspi + plateau + expi)
 int consigneNbCycle = 20;
 
-// degre d'ouverture de la valve brower (quantité d'air du blower qu'on push vers la boite 2)
-int consigneOuverture = 30; 
+// degré d'ouverture de la valve blower (quantité d'air du blower qu'on envoie vers la boite 2)
+int consigneOuverture = 30;
 
 // consigne de pression plateau maxi
-int consignePressionPlateauMax = 30; 
+int consignePressionPlateauMax = 30;
 
-// consigne de pression pep
-int consignePressionPEP = 5; 
+// consigne de pression PEP
+int consignePressionPEP = 5;
 
-// data pour affichage : donnée du cycle precedent pour ne pas afficher des datas aberrantes
+// données pour affichage (du cycle précédent pour ne pas afficher des valeurs aberrantes)
 int previousPressionCrete = -1;
 int previousPressionPlateau = -1;
 
 void loop() {
   int nbreCentiemeSecParCycle = 60 * 100 / consigneNbCycle;
-  int nbreCentiemeSecParInspi = nbreCentiemeSecParCycle / 3; // Inspiration est 1/3 du cycle, expi est 2/3
+  int nbreCentiemeSecParInspi = nbreCentiemeSecParCycle / 3; // inspiration = 1/3 du cycle, expiration = 2/3 du cycle
 
-//  Serial.println();
-//  Serial.println("------ Starting cycle ------");
-//  Serial.print("nbreCentiemeSecParCycle = "); 
-//  Serial.println(nbreCentiemeSecParCycle);
-//  Serial.print("nbreCentiemeSecParInspi = "); 
-//  Serial.println(nbreCentiemeSecParInspi);
+  //Serial.println();
+  //Serial.println("------ Starting cycle ------");
+  //Serial.print("nbreCentiemeSecParCycle = ");
+  //Serial.println(nbreCentiemeSecParCycle);
+  //Serial.print("nbreCentiemeSecParInspi = ");
+  //Serial.println(nbreCentiemeSecParInspi);
 
   int currentPressionCrete = -1;
   int currentPressionPlateau = -1;
-  
-  //int currentPositionBlower = secu_coupureBrower;
-  
-  int dureeBaissePression = 0; // compteur de centieme pour la détection du pic de pression (pression crete)
-  
+
+  //int currentPositionBlower = secu_coupureBlower;
+
+  int dureeBaissePression = 0; // compteur de centièmes pour la détection du pic de pression (pression crête)
+
   int currentPhase = PHASE_PUSH_INSPI;
 
   int positionBlower = 90;
@@ -69,64 +69,83 @@ void loop() {
 
   int consigneBlower = 90;
   int consignePatient = 90;
-  
-  for(int currentCentieme = 0; currentCentieme < nbreCentiemeSecParCycle; currentCentieme++){
-    
-    // mesure pression pour retro action
-    int currentPression = 0 ; // analogRead(4);
-    if(currentCentieme < 50){
-      currentPression = 100 ; //analogRead(PIN_CAPTEUR_PRESSION);
-    } else{
+
+  /********************************************/
+  // Début d'un cycle
+  /********************************************/
+  for (int currentCentieme = 0; currentCentieme < nbreCentiemeSecParCycle; currentCentieme++) {
+
+    /********************************************/
+    // Mesure pression pour rétro-action
+    /********************************************/
+    int currentPression = 0 ; //analogRead(PIN_CAPTEUR_PRESSION);
+    if (currentCentieme < 50) {
+      currentPression = 100; //analogRead(PIN_CAPTEUR_PRESSION);
+    } else {
       currentPression = 90;
     }
 
-    // calcul des consignes normales
-    if(currentCentieme <= nbreCentiemeSecParInspi){
-      if(currentPression >= currentPressionCrete){
+    /********************************************/
+    // Calcul des consignes normales
+    /********************************************/
+    if (currentCentieme <= nbreCentiemeSecParInspi) {
+      if (currentPression >= currentPressionCrete) {
         currentPhase = PHASE_PUSH_INSPI;
         currentPressionCrete = currentPression;
-        
+
         consigneBlower = 90 - consigneOuverture;  // on ouvre le blower vers patient à la consigne paramétrée
         consignePatient = 90 + ANGLE_OUVERTURE_MAXI; // on ouvre le flux IN patient
-      } else{
-        currentPhase = PHASE_HOLD_INSPI; 
+      } else {
+        currentPhase = PHASE_HOLD_INSPI;
         currentPressionPlateau = currentPression;
- 
+
         consigneBlower = 90 + ANGLE_OUVERTURE_MAXI; // on shunt vers l'extérieur
         consignePatient = 90; // on bloque les flux patient
       }
-    } else{ // on gère l'expiration on est phase PHASE_EXPIRATION
+    } else { // on gère l'expiration on est phase PHASE_EXPIRATION
       currentPhase = PHASE_EXPIRATION;
       consigneBlower = 90 + ANGLE_OUVERTURE_MAXI;  // on shunt vers l'extérieur
-      consignePatient = secu_ouvertureExpi; // on ouvre le flux OUT patient (expi -> extérieur)
+      consignePatient = secu_ouvertureExpi; // on ouvre le flux OUT patient (expiration vers l'extérieur)
     }
 
-  /* 
-   * calcul des consignes de mise en sécurité du patient 
-   * -> si pression crete > max => fermeture ouverture blower de 2°
-   * -> si pression plateau > consigne param => ouverture expi de 1°
-   * -> si pression pep < pep mini => fermeture valve expiration complètement
-   */
-  
-//    if(currentCentieme % 10 == 0){
-//      Serial.print("Phase : ");
-//      Serial.println(currentPhase);
-//    }
+    /********************************************/
+    // Calcul des consignes de mise en sécurité
+    /********************************************/
+    /*
+    * -> si pression crête > max, alors fermeture ouverture blower de 2°
+    * -> si pression plateau > consigne param, alors ouverture expiration de 1°
+    * -> si pression PEP < PEP mini, alors fermeture complète valve expiration
+    */
 
-    if(consigneBlower != positionBlower){
+    //if (currentCentieme % 10 == 0) {
+    //  Serial.print("Phase : ");
+    //  Serial.println(currentPhase);
+    //}
+
+    /********************************************/
+    // Envoi des nouvelles valeurs aux actionneurs
+    /********************************************/
+    if (consigneBlower != positionBlower) {
       blower.write(consigneBlower);
       positionBlower = consigneBlower;
     }
 
-    if(consignePatient != positionPatient){
+    if (consignePatient != positionPatient) {
       patient.write(consignePatient);
       positionPatient = consignePatient;
     }
-    
+
+    /********************************************/
+    // Préparation des valeurs pour affichage
+    /********************************************/
     previousPressionCrete = currentPressionCrete;
     previousPressionPlateau = currentPressionPlateau;
-    
-    delay(10); // on attends 1 centieme de seconde (on aura de la dérive en temps, sera corrigé par rtc au besoin)
+
+    delay(10); // on attend 1 centième de seconde (on aura de la dérive en temps, sera corrigé par rtc au besoin)
+
   }
+  /********************************************/
+  // Fin du cycle
+  /********************************************/
 
 }
