8daf71eaca4c670ba2c6be0ed078a95df6c124cd
Clément HUBER <clement.huber@navya.tech>
2020-03-26 17:22:46 +0100

    Create distinctive files for the main features

---
+++ src/arduino/affichage.h
@@ -0,0 +1,13 @@
+/*=============================================================================
+ * @file affichage.h
+ *
+ * COVID Respirator
+ *
+ * @section copyright Copyright
+ *
+ * Makers For Life
+ *
+ * @section descr File description
+ *
+ * Fichier définissant les fonctionnalitées liés à l'affichage
+ */
