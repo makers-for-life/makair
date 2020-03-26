cd0328571a00648809dc6c3a74b134c6d61bb69b
Clément HUBER <clement.huber@navya.tech>
2020-03-26 18:43:04 +0100

    Create display functions in affichage and refactor code

---
+++ src/arduino/affichage.cpp
@@ -0,0 +1,110 @@
+/*=============================================================================
+ * @file affichage.cpp
+ *
+ * COVID Respirator
+ *
+ * @section copyright Copyright
+ *
+ * Makers For Life
+ *
+ * @section descr File description
+ *
+ * Fichier implémentant les fonctionnalitées liés à l'affichage
+ */
+
+// INCLUDES ===================================================================
+
+// Associated header
+#include "affichage.h"
+
+// Other libraries
+#include <LiquidCrystal.h>
+
+// FUNCTIONS ==================================================================
+
+void displayEveryCycle(LiquidCrystal& screen,
+                       ScreenSize size,
+                       int peakPressure,
+                       int plateauPressure,
+                       int peep)
+{
+  screen.setCursor(0, 0);
+
+  switch (size)
+  {
+  case ScreenSize::CHARS_16:
+  {
+      screen.print("pc");
+      screen.print(peakPressure);
+      screen.print("/pp");
+      screen.print(plateauPressure);
+      screen.print("/pep");
+      screen.print(peep);
+      screen.print("  ");
+      break;
+  }
+  case ScreenSize::CHARS_20:
+  {
+    screen.print("pc=");
+    screen.print(peakPressure);
+    screen.print("/pp=");
+    screen.print(plateauPressure);
+    screen.print("/pep=");
+    screen.print(peep);
+    screen.print("  ");
+    break;
+  }
+  default:
+  {
+    screen.print("pc");
+    screen.print(peakPressure);
+    screen.print("/pp");
+    screen.print(plateauPressure);
+    screen.print("/pep");
+    screen.print(peep);
+    screen.print("  ");
+  }
+  }
+}
+
+void displayDuringCycle(LiquidCrystal& screen,
+                        ScreenSize size,
+                        int cyclesPerMinute,
+                        int maxPlateauPressure,
+                        int peep,
+                        int currentPressure)
+{
+  screen.setCursor(0, 1);
+
+  switch (size) {
+  case ScreenSize::CHARS_16: {
+    screen.print("c");
+    screen.print(cyclesPerMinute);
+    screen.print("/pl");
+    screen.print(maxPlateauPressure);
+    screen.print("/pep");
+    screen.print(peep);
+    break;
+  }
+  case ScreenSize::CHARS_20: {
+    screen.print("c=");
+    screen.print(cyclesPerMinute);
+    screen.print("/pl=");
+    screen.print(maxPlateauPressure);
+    screen.print("/pep=");
+    screen.print(peep);
+    screen.print("|");
+    screen.print(currentPressure);
+    break;
+  }
+  default:
+  {
+    screen.print("c");
+    screen.print(cyclesPerMinute);
+    screen.print("/pl");
+    screen.print(maxPlateauPressure);
+    screen.print("/pep");
+    screen.print(peep);
+  }
+  }
+}
