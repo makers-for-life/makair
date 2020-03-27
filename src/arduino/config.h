c604be875f616de0eb7b51cd950c2e794f3c461d
Clément HUBER <clement.huber@navya.tech>
2020-03-27 09:17:37 +0100

    Remove deprecated variable

--- src/arduino/config.h
+++ src/arduino/config.h
@@ -22,14 +22,6 @@
 #define DEBUG
 
 /*
- * Deux versions de LCD sont supportees, les LCD 16 colonnes et 2 lignes (16x2)
- * et les LCD 20 colonnes et 4 lignes (20x4).
- * Lorsque LCD_20_CHARS est defini, un LCD 20x4 est utilise. Sinon il
- * s'agit d'un LCD 16x2
- */
-//#define LCD_20_CHARS
-
-/*
  * Lorsque SIMULATION est defini, l'information n'est pas lue du capteur de
  * pression mais generee logiciellement
  */
