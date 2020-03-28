baccc2f6b76e29806b015cc35b3367abe2ef2f45
Clément HUBER <clement.huber@navya.tech>
2020-03-28 16:58:43 +0100

    Squash all refactoring commits

---
+++ src/arduino/config.h
@@ -0,0 +1,25 @@
+/*=============================================================================
+ * @file config.h
+ *
+ * COVID Respirator
+ *
+ * @section copyright Copyright
+ *
+ * Makers For Life
+ *
+ * @section descr File description
+ *
+ * This file configures the build, given various possible configurations
+ */
+
+#pragma once
+
+//! When DEBUG is defined, some additional code sending infos is built
+#define DEBUG
+
+/*! When SIMULATION is defined, the pressure is not received from the sensor but artificially
+ *  generated
+ */
+//#define SIMULATION
+
+//#define __AVR_ATmega328P__
