6202510777632c673214c1615e537fb6e9fb7542
David Sferruzza <david.sferruzza@gmail.com>
2020-04-05 00:44:38 +0200

    Improve alarm documentation

--- src/arduino/alarm.cpp
+++ src/arduino/alarm.cpp
@@ -1,31 +1,32 @@
-/*=============================================================================
+/*
+    Copyright (C) 2020 Makers For Life
+*/
+/******************************************************************************
+ * @author Makers For Life
  * @file alarm.cpp
- *
- * COVID Respirator
- *
- * @section copyright Copyright
- *
- * Makers For Life
- *
- * @section descr File description
- *
- * This file is the implementation of Alarms
- */
+ * @brief Alarm related functions
+ *****************************************************************************/
 
 #pragma once
 
-/// Externals
+// INCLUDES ===================================================================
 
+// Externals
 #include "Arduino.h"
 
-/// Internals
-
+// Internals
 #include "alarm.h"
 #include "parameters.h"
 
-/* Definition of all bip duration */
-/* 4 TICK = 1 ms  ( not possible to have 1TICK = 1millisec because prescalor is 16bit anf input
-frequency eitehr 84 or 100 MHz*/
+// PROGRAM =====================================================================
+
+/**
+ * @name Definition of bips durations
+ * 4 ticks = 1 ms
+ * @warning it is not possible to have 1 tick = 1 ms because prescaler is 16 bits and input
+ * frequency either 84 or 100 MHz
+ */
+///@{
 #define TIMER_TICK_PER_MS 4
 #define BIP (100 * TIMER_TICK_PER_MS)
 #define BIP_PAUSE BIP
@@ -34,21 +35,22 @@ frequency eitehr 84 or 100 MHz*/
 #define PAUSE_20S (20 * 1000 * TIMER_TICK_PER_MS)
 #define PAUSE_10S (10 * 1000 * TIMER_TICK_PER_MS)
 #define PAUSE_1S (1 * 1000 * TIMER_TICK_PER_MS)
+///@}
 
-/* Yellow Alarm pattern size */
+/// Yellow alarm pattern size
 #define ALARM_YELLOW_SIZE 8
 
-/* Yellow alarm pattern definition, composed of multiple couple of states (Actif/Inactif) and
- * duration (miliseconds) */
+/// Yellow alarm pattern definition, composed of multiple couple of states (Actif/Inactif) and
+/// duration (miliseconds)
 const uint32_t Alarm_Yellow[ALARM_YELLOW_SIZE] = {
     TIMER_OUTPUT_COMPARE_FORCED_ACTIVE, BEEEEP, TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, BEEEEP_PAUSE,
     TIMER_OUTPUT_COMPARE_FORCED_ACTIVE, BEEEEP, TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, PAUSE_20S};
 
-/* Red Alarm pattern size */
+/// Red alarm pattern size
 #define ALARM_RED_SIZE 32
 
-/* Red alarm pattern definition, composed of multiple couple of states (Actif/Inactif) and duration
- * (miliseconds) */
+/// Red alarm pattern definition, composed of multiple couple of states (Actif/Inactif) and duration
+/// (miliseconds)
 const uint32_t Alarm_Red[ALARM_RED_SIZE] = {
     TIMER_OUTPUT_COMPARE_FORCED_ACTIVE, BIP,    TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, BIP_PAUSE,
     TIMER_OUTPUT_COMPARE_FORCED_ACTIVE, BIP,    TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, BIP_PAUSE,
@@ -59,17 +61,16 @@ const uint32_t Alarm_Red[ALARM_RED_SIZE] = {
     TIMER_OUTPUT_COMPARE_FORCED_ACTIVE, BIP,    TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, BIP_PAUSE,
     TIMER_OUTPUT_COMPARE_FORCED_ACTIVE, BEEEEP, TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, PAUSE_10S};
 
-/* Boot Alarm pattern size */
+/// Boot alarm pattern size
 #define ALARM_BOOT_SIZE 4
 
-/* Yellow alarm pattern definition, composed of multiple couple of states (Actif/Inactif) and
- * duration (miliseconds) */
+/// Yellow alarm pattern definition, composed of multiple couple of states (Actif/Inactif) and
+/// duration (miliseconds)
 const uint32_t Alarm_Boot[ALARM_BOOT_SIZE] = {TIMER_OUTPUT_COMPARE_FORCED_ACTIVE, BEEEEP,
                                               TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, BEEEEP_PAUSE};
 
 // INITIALISATION =============================================================
 
-/* variables definition */
 const uint32_t* Active_Alarm = nullptr;
 uint32_t Active_Alarm_Index = 0;
 uint32_t Active_Alarm_Size = 2;
@@ -79,7 +80,7 @@ uint32_t AlarmTimchannel;
 
 /**
  * When timer period expires, switch to next state in the pattern of the alarm
- * Not: API update since version 1.9.0 of Arduino_Core_STM32
+ * @note API update since version 1.9.0 of Arduino_Core_STM32
  */
 #if (STM32_CORE_VERSION < 0x01090000)
 void Update_IT_callback(HardwareTimer*)  // NOLINT(readability/casting)
@@ -87,18 +88,14 @@ void Update_IT_callback(HardwareTimer*)  // NOLINT(readability/casting)
 void Update_IT_callback(void)
 #endif
 {
-    /* patterns are composed of multiple couple of states (Actif/Inactif) and duration
-     * (miliseconds)*/
-    /* Previous state is finished, switch to next one */
+    // Patterns are composed of multiple couple of states (Actif/Inactif) and duration (miliseconds)
+    // Previous state is finished, switch to next one
     AlarmTim->setMode(AlarmTimchannel, (TimerModes_t)Active_Alarm[Active_Alarm_Index], PIN_ALARM);
     AlarmTim->setOverflow(Active_Alarm[Active_Alarm_Index + 1], TICK_FORMAT);
     Active_Alarm_Index = (Active_Alarm_Index + 2) % Active_Alarm_Size;
     AlarmTim->resume();
 }
 
-/**
- * Initialization of HardwareTimer for Alarm purpose
- */
 void Alarm_Init() {
     // Automatically retrieve timer instance and channel associated with the pin
     // Useful in case of board change
@@ -110,59 +107,39 @@ void Alarm_Init() {
     AlarmTim = new HardwareTimer(Instance);
 
     AlarmTim->setMode(AlarmTimchannel, TIMER_OUTPUT_COMPARE_FORCED_INACTIVE, PIN_ALARM);
-    /* 4 TICK = 1 ms  ( not possible to have 1TICK = 1millisec because prescalor is 16bit anf input
-     * frequency eitehr 84 or 100 MHz*/
-    /* Use of TICK format o avoid computation within interrupt handler */
+    // 4 ticks = 1 ms  (it is not possible to have 1 tick = 1 ms because prescaler is 16 bit and
+    // input frequency either 84 or 100 MHz Use of tick format to avoid computation within interrupt
+    // handler
     AlarmTim->setPrescaleFactor(AlarmTim->getTimerClkFreq() / (TIMER_TICK_PER_MS * 1000));
-    AlarmTim->setOverflow(100 * TIMER_TICK_PER_MS, TICK_FORMAT);  // Default 100milisecondes
+    AlarmTim->setOverflow(100 * TIMER_TICK_PER_MS, TICK_FORMAT);  // Default 100 ms
 
-    /* Start with inactive state without interruptions */
+    // Start with inactive state without interruptions
     AlarmTim->resume();
 }
 
-/**
- * Generic function to activate an alarm.
- * Input parameters: alarm pattern array and its size
- */
 void Alarm_Start(const uint32_t* Alarm, uint32_t Size) {
     AlarmTim->setCount(0);
     Active_Alarm = Alarm;
     Active_Alarm_Index = 0;
     Active_Alarm_Size = Size;
 
-    /* patterns are composed of multiple couple of states (Actif/Inactif) and duration (miliseconds)
-     */
-    /* Configuration of first etat of pattern */
+    // Patterns are composed of multiple couple of states (Actif/Inactif) and duration (miliseconds)
+    // Configuration of first state of pattern
     AlarmTim->setMode(AlarmTimchannel, (TimerModes_t)Active_Alarm[Active_Alarm_Index], PIN_ALARM);
     AlarmTim->setOverflow(Active_Alarm[Active_Alarm_Index + 1], TICK_FORMAT);
 
-    /* Activate interrupt callback to handle further states */
+    // Activate interrupt callback to handle further states
     AlarmTim->attachInterrupt(Update_IT_callback);
     Active_Alarm_Index = (Active_Alarm_Index + 2) % Active_Alarm_Size;
 
-    /* Time stars. Required to configure output on GPIO */
+    // Time stars. Required to configure output on GPIO
     AlarmTim->resume();
 }
 
-/**
- * Activate an yellow alarm.
- * Input parameters: alarm pattern array and its size
- */
 void Alarm_Yellow_Start(void) { Alarm_Start(Alarm_Yellow, ALARM_YELLOW_SIZE); }
 
-/**
- * Activate an red alarm.
- * Input parameters: alarm pattern array and its size
- */
 void Alarm_Red_Start(void) { Alarm_Start(Alarm_Red, ALARM_RED_SIZE); }
 
-/**
- * Activate boot bip.
- * Input parameters: alarm pattern array and its size
- */
 void Alarm_Boot_Start(void) { Alarm_Start(Alarm_Boot, ALARM_BOOT_SIZE); }
 
-/**
- * Stop Alarm
- */
 void Alarm_Stop(void) { AlarmTim->pause(); }
