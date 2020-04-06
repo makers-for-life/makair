d1978686441ac864828424632e04a43aa087d531
Baptiste Jamin <baptistejamin@gmail.com>
2020-04-06 14:16:30 +0200

    Remove shit

--- src/code/build/build_deploy_and_monitor.sh
+++ src/code/build/build_deploy_and_monitor.sh
@@ -2,6 +2,6 @@
 
 rm output/respirator*
 rm -rf /tmp/arduino-sketch-*
-arduino-cli compile --port tty.usbmodem143103 --fqbn STM32:stm32:Nucleo_64:pnum=NUCLEO_F411RE srcs/respirator.cpp --output output/respirator
-arduino-cli upload  --port tty.usbmodem143103 --fqbn STM32:stm32:Nucleo_64:pnum=NUCLEO_F411RE --input output/respirator
+arduino-cli compile --port ${BOARD_PORT} --fqbn STM32:stm32:Nucleo_64:pnum=NUCLEO_F411RE srcs/respirator.cpp --output output/respirator
+arduino-cli upload  --port ${BOARD_PORT}--fqbn STM32:stm32:Nucleo_64:pnum=NUCLEO_F411RE --input output/respirator
 pio device monitor -b 115200
