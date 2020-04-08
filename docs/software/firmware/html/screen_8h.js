920b550187a473fef6b6df5b923039290197a925
dsferruzza <david.sferruzza@gmail.com>
2020-04-08 16:58:43 +0000

    Deploying to gh-pages from  @ 6ef1aabe6dc5fa87759128e2b5f5bf7eb709949f 🚀

--- docs/software/firmware/html/screen_8h.js
+++ docs/software/firmware/html/screen_8h.js
@@ -1,8 +1,8 @@
 var screen_8h =
 [
-    [ "displayCurrentInformation", "screen_8h.html#ac2255b86b35de8eb463e1bf4de4641a0", null ],
-    [ "displaySettings", "screen_8h.html#ace7f3e2a80bab3a49c3ccfae1041869e", null ],
-    [ "displaySubPhase", "screen_8h.html#a318ffe865f35a9e1fab9ae5535b7f5c8", null ],
+    [ "displayCurrentInformation", "screen_8h.html#afbcfd6c7b111b47bfc4324e8f809d10c", null ],
+    [ "displayCurrentPressure", "screen_8h.html#add30ddd4887b877ea93057322a3d3c33", null ],
+    [ "displayCurrentSettings", "screen_8h.html#a61be08c2e2ab346aac63942c7624cf7e", null ],
     [ "resetScreen", "screen_8h.html#ae56ad795427d7a17af1755baeca56ca8", null ],
     [ "startScreen", "screen_8h.html#aea9312c2a12fc8e9fef16a9bdfd9b581", null ],
     [ "screen", "screen_8h.html#ae2ee4908ee32a266ef7908828279d9a1", null ]
