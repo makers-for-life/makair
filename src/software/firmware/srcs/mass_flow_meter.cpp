9307bcf803d6625e9f8d424ba0358a56300d9362
pi-r-p <pierre.papin@senx.io>
2020-04-13 23:08:53 +0200

    bootstrap the mass flow meter

--- src/software/firmware/srcs/mass_flow_meter.cpp
+++ src/software/firmware/srcs/mass_flow_meter.cpp
@@ -25,19 +25,29 @@
 // Linked to Hardware v2
 #ifdef MASS_FLOW_METER
 
-// 2 khz => prescaler = 50000 => OK for a 16 bit timer. 
-// we cannnot be slower
+// 2 khz => prescaler = 50000 => OK for a 16 bit timer. it cannnot be slower
+// 10 khz => nice
 #define MASS_FLOW_TIMER_FREQ 10000
+
+// the timer period in microsecond, 100us precision (10 khz)
+#define MASS_FLOW_PERIOD_US 1000
 
 HardwareTimer* massFlowTimer;
 
-
+// TO REMOVE
 boolean testcon = false;
 
+int32_t airVolumeSum = 0;
+
 void MFM_Timer_Callback(HardwareTimer*) {
-  //Serial.println("dtc");
-  testcon = !testcon;
-  digitalWrite(PIN_SERIAL_TX,testcon);
+
+    // TO REMOVE : measure period here.
+    testcon = !testcon;
+    digitalWrite(PIN_SERIAL_TX, testcon);
+
+    int16_t air = 150;
+
+    airVolumeSum += air;
 }
 
 /**
@@ -48,14 +58,20 @@ boolean MFM_init(void) {
 
     // set the timer
     massFlowTimer = new HardwareTimer(MASS_FLOW_TIMER);
-    
-    // prescaler. stm32f411 clock is 100mhz.
+
+    // prescaler. stm32f411 clock is 100mhz
     massFlowTimer->setPrescaleFactor((massFlowTimer->getTimerClkFreq() / MASS_FLOW_TIMER_FREQ) - 1);
 
-    // CPU Clock down to 10 kHz
-    massFlowTimer->setOverflow(10);  // @10khz, 1 millisecond
+    // set the period
+    massFlowTimer->setOverflow(MASS_FLOW_TIMER_FREQ / MASS_FLOW_PERIOD_US);
     massFlowTimer->setMode(MASS_FLOW_CHANNEL, TIMER_OUTPUT_COMPARE, NC);
     massFlowTimer->attachInterrupt(MFM_Timer_Callback);
+
+    // interrupt priority is documented here:
+    // https://stm32f4-discovery.net/2014/05/stm32f4-stm32f429-nvic-or-nested-vector-interrupt-controller/
+    massFlowTimer->setInterruptPriority(2, 0);
+
+    // start the timer
     massFlowTimer->resume();
 
     // detect if the sensor is connected
@@ -63,34 +79,41 @@ boolean MFM_init(void) {
 }
 
 /**
- * Reset the volume counter
- */
-void MFM_reset(void) {}
-
-/**
  * return the number of milliliters since last reset
- * Can also perform the reset in the same atomic operation
+ * Can also perform the volume reset in the same atomic operation
  */
-int32_t MFM_read_liters(boolean reset_after_read) { return 0; }
+int32_t MFM_read_liters(boolean reset_after_read) {
 
+    int32_t result;
+
+    // this should be an atomic operation (32 bits aligned data)
+    result = airVolumeSum;
+
+    if (reset_after_read) {
+        airVolumeSum = 0;
+    }
+
+    // compute the result in ml
+
+    return 0;
+}
 
 void setup(void) {
-  Serial.begin(115200);
-  Serial.println("coucou, tu veux voir ma ... ?");
-  Serial.println("init mass flow meter");
-  MFM_init();
+    Serial.begin(115200);
+    Serial.println("coucou, tu veux voir ma ... ?");
+    Serial.println("init mass flow meter");
+    MFM_init();
 
-  pinMode(PIN_SERIAL_TX,OUTPUT);
+    pinMode(PIN_SERIAL_TX, OUTPUT);
 
-  Serial.println("init done");
+    Serial.println("init done");
 }
 
 void loop(void) {
 
-  delay(1000);
-  Serial.print("volume = ");
-  Serial.println(MFM_read_liters(false));
-
+    delay(1000);
+    Serial.print("volume = ");
+    Serial.println(MFM_read_liters(false));
 }
 
 #endif
