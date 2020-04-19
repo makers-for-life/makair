0f94656beca12f879cc1ec36542cbb264315acb9
Andrew Tridgell <andrew@tridgell.net>
2020-04-20 07:00:16 +1000

    firmware: trivial typo fix
    missing space

--- src/software/firmware/build/build_deploy_and_monitor.sh
+++ src/software/firmware/build/build_deploy_and_monitor.sh
@@ -3,5 +3,5 @@
 rm output/respirator*
 rm -rf /tmp/arduino-sketch-*
 arduino-cli compile --port ${BOARD_PORT} --fqbn STM32:stm32:Nucleo_64:pnum=NUCLEO_F411RE srcs/respirator.cpp --output output/respirator
-arduino-cli upload  --port ${BOARD_PORT}--fqbn STM32:stm32:Nucleo_64:pnum=NUCLEO_F411RE --input output/respirator
+arduino-cli upload  --port ${BOARD_PORT} --fqbn STM32:stm32:Nucleo_64:pnum=NUCLEO_F411RE --input output/respirator
 pio device monitor -b 115200
