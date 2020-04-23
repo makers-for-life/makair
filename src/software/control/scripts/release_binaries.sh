e7295917678bf86526694d83ed4cbf9375e7d599
Valerian Saliou <valerian@valeriansaliou.name>
2020-04-23 19:02:32 +0200

    Hacky build script for now, but works
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/software/control/scripts/release_binaries.sh
+++ src/software/control/scripts/release_binaries.sh
@@ -60,8 +60,19 @@ rc=0
 pushd "$BASE_DIR" > /dev/null
     echo "Executing release steps for MakAir Control v$MAKAIR_CONTROL_VERSION..."
 
+    # Move the telemetry library into current working directory
+    # Notice: this feels a bit hacky, but for now 'cross' is not capable of passing \
+    #   parent directories to its Docker runtime.
+    mv ../telemetry ./
+    sed -i 's/path = "..\/telemetry"/path = ".\/repo\/src\/software\/telemetry"/' ./Cargo.toml
+
+    # Build releases
     release_for_architecture "armv7" "armv7-unknown-linux-musleabihf"
     rc=$?
+
+    # Move back the telemetry library
+    mv ./telemetry ../
+    sed -i 's/path = ".\/repo\/src\/software\/telemetry"/path = "..\/telemetry"/' ./Cargo.toml
 
     if [ $rc -eq 0 ]; then
         echo "Success: Done executing release steps for MakAir Control v$MAKAIR_CONTROL_VERSION"
