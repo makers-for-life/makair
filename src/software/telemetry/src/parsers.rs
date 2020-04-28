d162c27568a6f399cbe3b472b02192143d6cf123
Ronan Michaux <ronan_michaux@yahoo.com>
2020-04-28 19:21:34 +0200

    Finish telemetry_message_strategy()

--- src/software/telemetry/src/parsers.rs
+++ src/software/telemetry/src/parsers.rs
@@ -329,40 +329,154 @@ pub mod tests {
     }
 
     #[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
-    pub struct DeviceIdComponents(u32, u32, u32);
+    pub struct DeviceIdInts(u32, u32, u32);
 
-    impl DeviceIdComponents {
+    impl DeviceIdInts {
         fn str(&self) -> String {
             format!("{}-{}-{}", self.0, self.1, self.2)
         }
     }
 
     prop_compose! {
-        pub fn device_id_components_strategy()(id1 in (0u32..), id2 in (0u32..), id3 in (0u32..)) -> DeviceIdComponents {
-            DeviceIdComponents(id1, id2, id3)
+        pub fn device_id_ints_strategy()(id1 in (0u32..), id2 in (0u32..), id3 in (0u32..)) -> DeviceIdInts {
+            DeviceIdInts(id1, id2, id3)
         }
     }
 
     prop_compose! {
         fn boot_message_strategy()(
             version in ".*",
-            device_id in device_id_components_strategy(),
+            device_id in device_id_ints_strategy(),
             systick in (0u64..),
             mode in mode_strategy(),
             value128 in (0u8..),
-        ) -> (BootMessage, DeviceIdComponents) {
+        ) -> (BootMessage, DeviceIdInts) {
             (BootMessage { version, device_id: device_id.str(), systick, mode, value128 }, device_id)
         }
     }
 
+    prop_compose! {
+        fn stopped_message_strategy()(
+            version in ".*",
+            device_id in device_id_ints_strategy(),
+            systick in (0u64..),
+        ) -> (StoppedMessage, DeviceIdInts){
+            (StoppedMessage { version, device_id: device_id.str(), systick, }, device_id)
+        }
+    }
+
+    prop_compose! {
+        fn data_snapshot_message_strategy()(
+            version in ".*",
+            device_id in device_id_ints_strategy(),
+            systick in (0u64..),
+            centile in (0u16..),
+            pressure in (0u16..),
+            phase_subphase in phase_subphase_strategy(),
+            blower_valve_position in (0u8..),
+            patient_valve_position in (0u8..),
+            blower_rpm in (0u8..),
+            battery_level in (0u8..),
+        ) -> (DataSnapshot, DeviceIdInts, (Phase, SubPhase)){
+            (DataSnapshot {
+                version,
+                device_id: device_id.str(),
+                systick,
+                centile,
+                pressure,
+                phase: phase_subphase.0.clone(),
+                subphase: phase_subphase.1.clone(),
+                blower_valve_position,
+                patient_valve_position,
+                blower_rpm,
+                battery_level,
+            }, device_id, phase_subphase)
+        }
+    }
+
+    prop_compose! {
+        fn machine_state_message_strategy()(
+            version in ".*",
+            device_id in device_id_ints_strategy(),
+            systick in (0u64..),
+            cycle in (0u32..),
+            peak_command in (0u8..),
+            plateau_command in (0u8..),
+            peep_command in (0u8..),
+            cpm_command in (0u8..),
+            previous_peak_pressure in (0u16..),
+            previous_plateau_pressure in (0u16..),
+            previous_peep_pressure in (0u16..),
+            current_alarm_codes in collection::vec(0u8.., 0..100),
+        ) -> (MachineStateSnapshot, DeviceIdInts){
+            (MachineStateSnapshot {
+                version,
+                device_id: device_id.str(),
+                systick,
+                cycle,
+                peak_command,
+                plateau_command,
+                peep_command,
+                cpm_command,
+                previous_peak_pressure,
+                previous_plateau_pressure,
+                previous_peep_pressure,
+                current_alarm_codes,
+            }, device_id)
+        }
+    }
+
+    prop_compose! {
+        fn alarm_trap_message_strategy()(
+            version in ".*",
+            device_id in device_id_ints_strategy(),
+            systick in (0u64..),
+            centile in (0u16..),
+            pressure in (0u16..),
+            phase_subphase in phase_subphase_strategy(),
+            cycle in (0u32..),
+            alarm_code in (0u8..),
+            alarm_priority in alarm_priority_strategy(),
+            triggered in proptest::bool::ANY,
+            expected in (0u32..),
+            measured in (0u32..),
+            cycles_since_trigger in (0u32..),
+        ) -> (AlarmTrap, DeviceIdInts, (Phase, SubPhase)){
+            (AlarmTrap {
+                version,
+                device_id: device_id.str(),
+                systick,
+                centile,
+                pressure,
+                phase: phase_subphase.0.clone(),
+                subphase: phase_subphase.1.clone(),
+                cycle,
+                alarm_code,
+                alarm_priority,
+                triggered,
+                expected,
+                measured,
+                cycles_since_trigger,
+            }, device_id, phase_subphase)
+        }
+    }
+
     pub fn telemetry_message_strategy() -> impl Strategy<Value = TelemetryMessage> {
-        prop_oneof![boot_message_strategy().prop_map(|(msg, _)| TelemetryMessage::BootMessage(msg)),]
+        prop_oneof![
+            boot_message_strategy().prop_map(|(msg, _)| TelemetryMessage::BootMessage(msg)),
+            stopped_message_strategy().prop_map(|(msg, _)| TelemetryMessage::StoppedMessage(msg)),
+            data_snapshot_message_strategy()
+                .prop_map(|(msg, _, _)| TelemetryMessage::DataSnapshot(msg)),
+            machine_state_message_strategy()
+                .prop_map(|(msg, _)| TelemetryMessage::MachineStateSnapshot(msg)),
+            alarm_trap_message_strategy().prop_map(|(msg, _, _)| TelemetryMessage::AlarmTrap(msg)),
+        ]
     }
 
     proptest! {
         #[test]
-        fn test_boot_message_parser( msg_and_device_id in boot_message_strategy()) {
-            let (msg, device_id) = msg_and_device_id;
+        fn test_boot_message_parser(msg_and_extra_datas in boot_message_strategy()) {
+            let (msg, device_id) = msg_and_extra_datas;
 
             // This needs to be consistent with sendBootMessage() defined in src/software/firmware/srcs/telemetry.cpp
             let input = &flat(&[
@@ -388,27 +502,17 @@ pub mod tests {
 
     proptest! {
         #[test]
-        fn test_stopped_message_parser(
-            version in ".*",
-            device_id1 in (0u32..),
-            device_id2 in (0u32..),
-            device_id3 in (0u32..),
-            systick in (0u64..),
-        ) {
-            let msg = StoppedMessage {
-                version,
-                device_id: format!("{}-{}-{}", device_id1, device_id2, device_id3),
-                systick,
-            };
+        fn test_stopped_message_parser(msg_and_extra_datas in stopped_message_strategy()) {
+            let (msg, device_id) = msg_and_extra_datas;
 
             // This needs to be consistent with sendStoppedMessage() defined in src/software/firmware/srcs/telemetry.cpp
             let input = &flat(&[
                 b"O:\x01",
                 &[msg.version.len() as u8],
                 &msg.version.as_bytes(),
-                &device_id1.to_be_bytes(),
-                &device_id2.to_be_bytes(),
-                &device_id3.to_be_bytes(),
+                &device_id.0.to_be_bytes(),
+                &device_id.1.to_be_bytes(),
+                &device_id.2.to_be_bytes(),
                 b"\t",
                 &msg.systick.to_be_bytes(),
                 b"\n",
@@ -421,42 +525,17 @@ pub mod tests {
 
     proptest! {
         #[test]
-        fn test_data_snapshot_message_parser(
-            version in ".*",
-            device_id1 in (0u32..),
-            device_id2 in (0u32..),
-            device_id3 in (0u32..),
-            systick in (0u64..),
-            centile in (0u16..),
-            pressure in (0u16..),
-            phase_subphase in phase_subphase_strategy(),
-            blower_valve_position in (0u8..),
-            patient_valve_position in (0u8..),
-            blower_rpm in (0u8..),
-            battery_level in (0u8..),
-        ) {
-            let msg = DataSnapshot {
-                version,
-                device_id: format!("{}-{}-{}", device_id1, device_id2, device_id3),
-                systick,
-                centile,
-                pressure,
-                phase: phase_subphase.0.clone(),
-                subphase: phase_subphase.1.clone(),
-                blower_valve_position,
-                patient_valve_position,
-                blower_rpm,
-                battery_level,
-            };
+        fn test_data_snapshot_message_parser(msg_and_extra_datas in data_snapshot_message_strategy()) {
+             let (msg, device_id, (phase, subphase)) = msg_and_extra_datas;
 
             // This needs to be consistent with sendDataSnapshot() defined in src/software/firmware/srcs/telemetry.cpp
             let input = &flat(&[
                 b"D:\x01",
                 &[msg.version.len() as u8],
                 &msg.version.as_bytes(),
-                &device_id1.to_be_bytes(),
-                &device_id2.to_be_bytes(),
-                &device_id3.to_be_bytes(),
+                &device_id.0.to_be_bytes(),
+                &device_id.1.to_be_bytes(),
+                &device_id.2.to_be_bytes(),
                 b"\t",
                 &msg.systick.to_be_bytes(),
                 b"\t",
@@ -464,7 +543,7 @@ pub mod tests {
                 b"\t",
                 &msg.pressure.to_be_bytes(),
                 b"\t",
-                &[phase_value(phase_subphase.0, phase_subphase.1)],
+                &[phase_value(phase, subphase)],
                 b"\t",
                 &[msg.blower_valve_position],
                 b"\t",
@@ -483,45 +562,17 @@ pub mod tests {
 
     proptest! {
         #[test]
-        fn test_machine_state_snapshot_message_parser(
-            version in ".*",
-            device_id1 in (0u32..),
-            device_id2 in (0u32..),
-            device_id3 in (0u32..),
-            systick in (0u64..),
-            cycle in (0u32..),
-            peak_command in (0u8..),
-            plateau_command in (0u8..),
-            peep_command in (0u8..),
-            cpm_command in (0u8..),
-            previous_peak_pressure in (0u16..),
-            previous_plateau_pressure in (0u16..),
-            previous_peep_pressure in (0u16..),
-            current_alarm_codes in collection::vec(0u8.., 0..100),
-        ) {
-            let msg = MachineStateSnapshot {
-                version,
-                device_id: format!("{}-{}-{}", device_id1, device_id2, device_id3),
-                systick,
-                cycle,
-                peak_command,
-                plateau_command,
-                peep_command,
-                cpm_command,
-                previous_peak_pressure,
-                previous_plateau_pressure,
-                previous_peep_pressure,
-                current_alarm_codes,
-            };
+        fn test_machine_state_snapshot_message_parser(msg_and_extra_datas in machine_state_message_strategy()) {
+             let (msg, device_id) = msg_and_extra_datas;
 
             // This needs to be consistent with sendMachineStateSnapshot() defined in src/software/firmware/srcs/telemetry.cpp
             let input = &flat(&[
                 b"S:\x01",
                 &[msg.version.len() as u8],
                 &msg.version.as_bytes(),
-                &device_id1.to_be_bytes(),
-                &device_id2.to_be_bytes(),
-                &device_id3.to_be_bytes(),
+                &device_id.0.to_be_bytes(),
+                &device_id.1.to_be_bytes(),
+                &device_id.2.to_be_bytes(),
                 b"\t",
                 &msg.systick.to_be_bytes(),
                 b"\t",
@@ -553,48 +604,17 @@ pub mod tests {
 
     proptest! {
         #[test]
-        fn test_alarm_trap_message_parser(
-            version in ".*",
-            device_id1 in (0u32..),
-            device_id2 in (0u32..),
-            device_id3 in (0u32..),
-            systick in (0u64..),
-            centile in (0u16..),
-            pressure in (0u16..),
-            phase_subphase in phase_subphase_strategy(),
-            cycle in (0u32..),
-            alarm_code in (0u8..),
-            alarm_priority in alarm_priority_strategy(),
-            triggered in proptest::bool::ANY,
-            expected in (0u32..),
-            measured in (0u32..),
-            cycles_since_trigger in (0u32..),
-        ) {
-            let msg = AlarmTrap {
-                version,
-                device_id: format!("{}-{}-{}", device_id1, device_id2, device_id3),
-                systick,
-                centile,
-                pressure,
-                phase: phase_subphase.0.clone(),
-                subphase: phase_subphase.1.clone(),
-                cycle,
-                alarm_code,
-                alarm_priority,
-                triggered,
-                expected,
-                measured,
-                cycles_since_trigger,
-            };
+        fn test_alarm_trap_message_parser(msg_and_extra_datas in alarm_trap_message_strategy()) {
+             let (msg, device_id, (phase, subphase)) = msg_and_extra_datas;
 
             // This needs to be consistent with sendAlarmTrap() defined in src/software/firmware/srcs/telemetry.cpp
             let input = &flat(&[
                 b"T:\x01",
                 &[msg.version.len() as u8],
                 &msg.version.as_bytes(),
-                &device_id1.to_be_bytes(),
-                &device_id2.to_be_bytes(),
-                &device_id3.to_be_bytes(),
+                &device_id.0.to_be_bytes(),
+                &device_id.1.to_be_bytes(),
+                &device_id.2.to_be_bytes(),
                 b"\t",
                 &msg.systick.to_be_bytes(),
                 b"\t",
@@ -602,7 +622,7 @@ pub mod tests {
                 b"\t",
                 &msg.pressure.to_be_bytes(),
                 b"\t",
-                &[phase_value(phase_subphase.0, phase_subphase.1)],
+                &[phase_value(phase, subphase)],
                 b"\t",
                 &msg.cycle.to_be_bytes(),
                 b"\t",
